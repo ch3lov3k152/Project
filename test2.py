d17a4b574c63907b958359fbdcaea8bbc773f734
ch3lov3k152 <student5502@liceum15.ukr.education>
2025-04-24 08:47:17 +0300

    test GUI

---
+++ test2.py
@@ -0,0 +1,12 @@
+from tkinter import *
+
+win = Tk()
+win.title("My project")
+win.geometry("500x300")
+win.resizable(False, False)
+# Елементи керування
+# Написи
+lab = Label(win, text="Текст для напису", font=('Comic Sans MS', 20))
+nap.place(x=0.1, y=0.4)
+
+win.mainloop()
