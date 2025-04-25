f4da027fbe01fed1fce42a958e05c43f738485cf
ch3lov3k152 <student5502@liceum15.ukr.education>
2025-04-25 09:19:37 +0300

    test GUI, new

--- test2.py
+++ test2.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 
 win = Tk()
 win.title("My project")
@@ -6,7 +7,16 @@ win.geometry("500x300")
 win.resizable(False, False)
 # Елементи керування
 # Написи
-lab = Label(win, text="Текст для напису", font=('Comic Sans MS', 20))
-nap.place(x=0.1, y=0.4)
+nap = Label(win, text="Текст для напису", font=('Comic Sans MS', 20), bg="green", fg="white")
+nap.place(relx=0.2, rely=0.1, anchor="center")
+# Поле для введення
+ent = Entry(win, font=("Comic Sans MS", 20), width=4)
+ent.place(relx=0.1, rely=0.2)
+# Кнопка
+but= Button(win, text="Click", font=("Comic Sans MS", 20), width=7, height=3, fg="#00ff00", bg="blue")
+but.place(relx=0.1, rely=0.35)
+# Випадаючий список
+comb = ttk.Combobox(win, font=("Comic Sans MS", 15), width=10,values=['option1','option2','option3','option4','option5'])
+comb.place(relx=0.4, rely=0.35)
 
 win.mainloop()
